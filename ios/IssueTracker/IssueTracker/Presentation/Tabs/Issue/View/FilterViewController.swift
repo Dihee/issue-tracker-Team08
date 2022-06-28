677093bb15c0b932a98c2d0c2237042f97c86bbf
Jihee hwang <hwangjihee94@gmail.com>
2022-06-28 16:14:27 +0900

    temp

---
+++ ios/IssueTracker/IssueTracker/Presentation/Tabs/Issue/View/FilterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FilterViewController.swift
+//  IssueTracker
+//
+//  Created by Jihee hwang on 2022/06/28.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+    deinit {
+        print("Deinit: \(#fileID)")
+    }
+}
--- ios/IssueTracker/IssueTracker/Presentation/Tabs/Issue/View/IssueViewController.swift
+++ ios/IssueTracker/IssueTracker/Presentation/Tabs/Issue/View/IssueViewController.swift
@@ -8,9 +8,14 @@
 import SnapKit
 import UIKit
 
+protocol IssueViewDelegate: AnyObject {
+    func didClickFilterButton()
+}
+
 class IssueViewController: UIViewController {
     private let tableView = UITableView(frame: .zero, style: .plain)
     private let dataSource = TableviewDataSource()
+    weak var delegate: IssueViewDelegate?
 
     private let leftButton: UIButton = {
         let button = UIButton()
@@ -19,7 +24,7 @@ class IssueViewController: UIViewController {
         button.setTitleColor(UIColor.primary, for: .normal)
         return button
     }()
-    
+
     private let rightButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
@@ -29,21 +34,13 @@ class IssueViewController: UIViewController {
         return button
     }()
 
-    private let searchController: UISearchController = {
-        let searchController = UISearchController()
-        return searchController
-    }()
+    private let searchController = UISearchController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
         configureView()
-        view.backgroundColor = .white
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightButton)
-        navigationController?.navigationBar.prefersLargeTitles = true
-
-        navigationItem.title = "이슈"
+        registerAction()
     }
 
     deinit {
@@ -51,7 +48,16 @@ class IssueViewController: UIViewController {
     }
 
     private func configureView() {
+        view.backgroundColor = .white
+        delegate = self
+
+        navigationItem.title = "이슈"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightButton)
+        navigationController?.navigationBar.prefersLargeTitles = true
+
         tableView.dataSource = dataSource
+        tableView.delegate = self
         tableView.register(IssueListCell.self, forCellReuseIdentifier: IssueListCell.identifier)
     }
 
@@ -61,5 +67,30 @@ class IssueViewController: UIViewController {
         tableView.snp.makeConstraints {
             $0.top.bottom.leading.trailing.equalTo(view.safeAreaLayoutGuide)
         }
+    }
+
+    private func registerAction() {
+        leftButton.addAction(.init(handler: { [weak self] _ in
+            self?.delegate?.didClickFilterButton()
+        }), for: .touchUpInside)
+    }
+}
+
+extension IssueViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+
+    func scrollViewWillBeginDragging(_: UIScrollView) {
+        navigationItem.searchController = searchController
+    }
+}
+
+extension IssueViewController: IssueViewDelegate {
+    func didClickFilterButton() {
+        let netxtViewController = FilterViewController()
+        netxtViewController.modalPresentationStyle = UIModalPresentationStyle.automatic
+
+        present(netxtViewController, animated: true, completion: nil)
     }
 }
