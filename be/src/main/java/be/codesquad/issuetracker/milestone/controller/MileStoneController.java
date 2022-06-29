c53089e13f3afa2cba738e3426a02b0ba6c63aaf
donggi-lee-bit <devdonggilee@gmail.com>
2022-06-29 17:37:53 +0900

    feat: 마일스톤 생성 API 구현

--- be/src/main/java/be/codesquad/issuetracker/milestone/controller/MileStoneController.java
+++ be/src/main/java/be/codesquad/issuetracker/milestone/controller/MileStoneController.java
@@ -3,12 +3,16 @@ package be.codesquad.issuetracker.milestone.controller;
 import be.codesquad.issuetracker.issue.domain.Status;
 import be.codesquad.issuetracker.milestone.dto.MileStoneDetailResponse;
 import be.codesquad.issuetracker.milestone.dto.MileStoneListResponse;
+import be.codesquad.issuetracker.milestone.dto.MileStoneSaveRequest;
 import be.codesquad.issuetracker.milestone.service.MileStoneService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +27,13 @@ public class MileStoneController {
     public MileStoneListResponse getMileStones(String status) {
         log.debug("status: {}", status);
         return mileStoneService.findAll(Status.of(status));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createMileStone(MileStoneSaveRequest mileStoneSaveRequest) {
+        log.debug("mileStoneSaveRequest: {}", mileStoneSaveRequest);
+        mileStoneService.save(mileStoneSaveRequest);
     }
 
     @GetMapping("/{id}")
