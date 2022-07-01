003af0830dc5af04300c3f04a2189c0db046de4d
donggi-lee-bit <devdonggilee@gmail.com>
2022-07-01 14:06:38 +0900

    feat: milestone 수정 api 구현

--- be/src/main/java/be/codesquad/issuetracker/milestone/controller/MileStoneController.java
+++ be/src/main/java/be/codesquad/issuetracker/milestone/controller/MileStoneController.java
@@ -3,15 +3,18 @@ package be.codesquad.issuetracker.milestone.controller;
 import be.codesquad.issuetracker.issue.domain.Status;
 import be.codesquad.issuetracker.milestone.dto.MileStoneDetailResponse;
 import be.codesquad.issuetracker.milestone.dto.MileStoneListResponse;
-import be.codesquad.issuetracker.milestone.dto.MileStoneSaveRequest;
+import be.codesquad.issuetracker.milestone.dto.MilestoneEditResponse;
+import be.codesquad.issuetracker.milestone.dto.MilestoneRequest;
 import be.codesquad.issuetracker.milestone.service.MileStoneService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +35,9 @@ public class MileStoneController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createMileStone(MileStoneSaveRequest mileStoneSaveRequest) {
-        log.debug("mileStoneSaveRequest: {}", mileStoneSaveRequest);
-        mileStoneService.save(mileStoneSaveRequest);
+    public void createMileStone(MilestoneRequest milestoneRequest) {
+        log.debug("mileStoneSaveRequest: {}", milestoneRequest);
+        mileStoneService.save(milestoneRequest);
     }
 
     @GetMapping("/{id}")
@@ -45,5 +48,12 @@ public class MileStoneController {
     @DeleteMapping("/{id}")
     public void deleteMileStone(@PathVariable Long id) {
         mileStoneService.delete(id);
+    }
+
+    @PatchMapping("/{id}")
+    public MilestoneEditResponse editMilestone(
+        @PathVariable Long id,
+        @RequestBody MilestoneRequest milestoneRequest) {
+        return new MilestoneEditResponse(mileStoneService.edit(id, milestoneRequest));
     }
 }
--- be/src/main/java/be/codesquad/issuetracker/milestone/service/MileStoneService.java
+++ be/src/main/java/be/codesquad/issuetracker/milestone/service/MileStoneService.java
@@ -1,11 +1,11 @@
 package be.codesquad.issuetracker.milestone.service;
 
 import be.codesquad.issuetracker.issue.domain.Status;
-import be.codesquad.issuetracker.milestone.domain.MileStone;
+import be.codesquad.issuetracker.milestone.domain.Milestone;
 import be.codesquad.issuetracker.milestone.dto.MileStoneDetailResponse;
 import be.codesquad.issuetracker.milestone.dto.MileStoneListResponse;
-import be.codesquad.issuetracker.milestone.dto.MileStoneSaveRequest;
-import be.codesquad.issuetracker.milestone.repository.MileStoneRepository;
+import be.codesquad.issuetracker.milestone.dto.MilestoneRequest;
+import be.codesquad.issuetracker.milestone.repository.MilestoneRepository;
 import java.util.List;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
@@ -16,33 +16,45 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MileStoneService {
 
-    private final MileStoneRepository mileStoneRepository;
+    private final MilestoneRepository milestoneRepository;
 
     @Transactional(readOnly = true)
     public MileStoneListResponse findAll(Status status) {
-        List<MileStone> milestones = mileStoneRepository.findAllByStatus(status);
+        List<Milestone> milestones = milestoneRepository.findAllByStatus(status);
         return new MileStoneListResponse(milestones);
     }
 
     @Transactional(readOnly = true)
     public MileStoneDetailResponse findById(Long id) {
-        MileStone findMileStone = findByIdOrThrow(id);
-        return new MileStoneDetailResponse(findMileStone);
+        Milestone findMilestone = findByIdOrThrow(id);
+        return new MileStoneDetailResponse(findMilestone);
     }
 
     @Transactional
-    public void save(MileStoneSaveRequest mileStoneSaveRequest) {
-        MileStone mileStone = new MileStone(mileStoneSaveRequest);
-        mileStoneRepository.save(mileStone);
+    public void save(MilestoneRequest mileStoneSaveRequest) {
+        Milestone milestone = new Milestone(mileStoneSaveRequest);
+        milestoneRepository.save(milestone);
     }
 
+    @Transactional
     public void delete(Long id) {
-        MileStone findMilestone = findByIdOrThrow(id);
-        mileStoneRepository.delete(findMilestone);
+        Milestone findMilestone = findByIdOrThrow(id);
+        milestoneRepository.delete(findMilestone);
     }
 
-    private MileStone findByIdOrThrow(Long id) {
-        return mileStoneRepository.findById(id)
+    @Transactional
+    public Milestone edit(Long id, MilestoneRequest milestoneRequest) {
+        Milestone findMilestone = findByIdOrThrow(id);
+        findMilestone.editMilestone(
+            milestoneRequest.getTitle(),
+            milestoneRequest.getDescription(),
+            milestoneRequest.getDueDate(),
+            milestoneRequest.getStatus());
+        return findMilestone;
+    }
+
+    private Milestone findByIdOrThrow(Long id) {
+        return milestoneRepository.findById(id)
             .orElseThrow(NoSuchElementException::new);
     }
 }
